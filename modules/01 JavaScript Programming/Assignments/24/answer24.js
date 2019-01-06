a8c0c5d54212f91231e7410b71b4ae2f6855500f
mariyastash <mariyastashevski@gmail.com>
2019-01-06 20:07:07 +0200

    validation added

--- modules/01 JavaScript Programming/Assignments/24/answer24.js
+++ modules/01 JavaScript Programming/Assignments/24/answer24.js
@@ -1,27 +1,31 @@
 function RandomWord(arr) {
     this.arr = arr;
 
-    if(!Array.isArray(arr)){
-        return '"chars" should be an array';
-    }
     this.randomize = function (length) {
         var chars = arr.join("");
         var randomString = "";
 
-        if(isNaN(length)){
+        if (!Array.isArray(this.arr)) {
+            return '"chars" should be an array';
+        }
+    
+        for (var i = 0; i < arr.length; i++) {
+            if (!isNaN(arr[i])) {
+                return '"chars" should include only strings';
+            }
+        }
+
+        if (isNaN(length)) {
             return '"length" should be a number';
         }
         for (var i = 0; i < length; i++) {
-            if(! isNaN(chars[i])){
-                return '"chars" should include only strings';
-            }
             randomString += chars.charAt(Math.floor(Math.random() * arr.length));
         }
         return randomString;
     }
 }
 
-var word = new RandomWord(['a','b','g','u', 'a', 'k']);
+var word = new RandomWord(['a', 'b', 's', 'z', 'a', 'k']);
 var result = word.randomize(10);
 console.log(result);
 result = word.randomize(2);
