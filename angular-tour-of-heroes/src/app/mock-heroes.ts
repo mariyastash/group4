4a5072e3119db23a42ca09f5c589c0dca66774de
mariyastash <mariyastashevski@gmail.com>
2019-04-07 16:55:21 +0300

    angular tutorial

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'SuperMan'},
+    {id: 12, name: 'Batman'},
+    {id: 13, name: 'Bombasto'},
+    {id: 14, name: 'Celeritas'},
+    {id: 15, name: 'Magneta'},
+    {id: 16, name: 'RubberMan'},
+    {id: 17, name: 'Dynama'},
+    {id: 18, name: 'IronMan'},
+    {id: 19, name: 'Magma'},
+    {id: 20, name: 'SpiderMan'},
+];
